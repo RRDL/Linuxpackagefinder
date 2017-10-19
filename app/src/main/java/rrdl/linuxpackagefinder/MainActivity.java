954234d19357115b35d0f64e6c762eb0225b0d6c
Moneem saadaoui <contact.moneem@gmail.com>
2017-10-19 12:01:29 +0100

    Initial commit

--- app/src/main/java/rrdl/linuxpackagefinder/MainActivity.java
+++ app/src/main/java/rrdl/linuxpackagefinder/MainActivity.java
@@ -1,21 +1,18 @@
 package rrdl.linuxpackagefinder;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,43 +21,29 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    int i = 0;
-    EditText E;
     ListView l;
-    ArrayAdapter adapter;
-    ArrayList<String> lines;
-    ImageButton I;
+    EditText S;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        I=(ImageButton)findViewById(R.id.imageButton);
-        I.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder Bld=new AlertDialog.Builder(MainActivity.this);
-                Bld.setMessage("Linux Package finder created by LightWeight Solutions ® 2017 - source code available on LightWeight Solutions' github");
-                Bld.setCancelable(false);
-                Bld.setPositiveButton("OK",new DialogInterface.OnClickListener(){public void onClick(DialogInterface dialog,int id){dialog.cancel();}});
-                AlertDialog A=Bld.create();
-                A.show();
-            }
-        });
+
+        Toast.makeText(getApplicationContext(),"Total available packages : 57957",Toast.LENGTH_SHORT).show();
+
         try {
 
-            StringBuilder buf = new StringBuilder();
             InputStream stream = getBaseContext().getAssets().open("packages.txt");
             BufferedReader in = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
             String str;
-            ArrayList<String> lines = new ArrayList<String>();
+            ArrayList<String> lines = new ArrayList<>();
 
             while ((str = in.readLine()) != null ) {
                 lines.add(str);
             }
             in.close();
-            E = (EditText) findViewById(R.id.editText);
             l = (ListView) findViewById(R.id.List);
-            final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.item, R.id.textView, lines);
+            final ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.item, R.id.textView, lines);
             l.setAdapter(adapter);
             l.setTextFilterEnabled(true);
             l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -75,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             });
-            E.addTextChangedListener(new TextWatcher() {
+            S =(EditText)findViewById(R.id.editText);
+            S.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -83,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
-                   adapter.getFilter().filter(s);
+                adapter.getFilter().filter(S.getText().toString());
                 }
 
                 @Override
@@ -91,11 +75,12 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             });
+
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.i("exception",e.toString());
         }
 
 
+        }
 
-    }
 }
